3f96c42c8aefbe1f27aae178e400dbcb3ab3b1f3
pankaj bisht <pankajbisht031@gmail.com>
2014-07-26 01:29:31 +0530

    text and html functionality added

--- js/query.js
+++ js/query.js
@@ -1,15 +1,80 @@
 window.N = (function () {
     function Dome (els) {
+        
         for(var i = 0; i < els.length; i++ ) {
             this[i] = els[i];
         }
+        
         this.length = els.length;     
     }
+    
+    Dome.prototype.map = function (callback) {
+        
+        var results = [], i = 0;
+        
+        T.info(this);
+        
+        for ( ; i < this.length; i++) {
+            results.push(callback.call(this, this[i], i));
+        }
+        
+        T.dir(results);
+    
+        return results;
+    };
+    
+    Dome.prototype.forEach = function (callback) {
+        
+        this.map(callback);
+        return this;
+    };
+    
+    Dome.prototype.mapOne = function (callback) {
+        
+        var m = this.map(callback);
+        return m.length > 1 ? m : m[0];
+    };
+    
+    Dome.prototype.text = function (text) {
+        
+        if (typeof text !== "undefined") {
+            
+            return this.forEach(function (el) {
+                el.innerText = text;
+            });
+        } else {
+            
+            return this.mapOne(function (el) {
+                return el.innerText;
+            });
+        }
+    };
+    
+    Dome.prototype.html = function (html) {
+        
+        if (typeof html !== "undefined") {
+        
+            this.forEach(function (el) {
+                el.innerHTML = html;
+            });
+        
+            return this;
+        } else {
+        
+            return this.mapOne(function (el) {
+                return el.innerHTML;
+            });
+        }
+    };
      
     var N = {
+        
         get: function (selector) {
-            T.log(selector);
+            
+            T.info(selector);
+            
             var els;
+            
             if (typeof selector === "string") {
                 els = document.querySelectorAll(selector);
             } else if (selector.length) {
@@ -17,6 +82,9 @@ window.N = (function () {
             } else {
                 els = [selector];
             }
+            
+            T.dir(els);
+            
             return new Dome(els);
         }
     };
--- js/test.js
+++ js/test.js
@@ -1,8 +1,26 @@
 window.T = (function () {
+    
     var test = {
+        
+        /*
+         * This is for my log information
+         * which is used in my developer tool to see
+         */
         log: function(msg) {
+         
             console.log(msg)
+        },
+        
+        info: function(msg) {
+         
+            console.info(msg);
+        },
+        
+        dir: function(msg) {
+            
+            console.dir(msg);
         }
     }
+    
     return test;
 }());
